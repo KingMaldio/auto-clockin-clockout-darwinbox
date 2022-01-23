71823e9be63a17b76fe0ea92967c5f912105ae0f
Akmal <32015620+KingMaldio@users.noreply.github.com>
2022-01-23 10:24:49 +0700

    Update script_template.sh

--- script_template.sh
+++ script_template.sh
@@ -3,5 +3,8 @@
 # <path-to-env> example: /Users/yourname/Documents/auto-darwin/env/bin/activate (mac)
 source <path-to-env>
 
+pip install selenium
+pip install webdriver_manager
+
 # <path-to-auto-darwin> example: /Users/yourname/Documents/auto-darwin/main.py
-python3 <path-to-auto-darwin>
+python <path-to-auto-darwin>
